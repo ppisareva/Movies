c19865e123aa085f6b1f3d7d79704856d7d1099a
polina <polina.pisareva@gmail.com>
2017-09-16 13:38:34 -0700

    make Heterogenous Layouts inside RecyclerView improved design

--- app/src/main/java/polina/example/com/movies/Activities/MainListActivity.java
+++ app/src/main/java/polina/example/com/movies/Activities/MainListActivity.java
@@ -1,7 +1,12 @@
 package polina.example.com.movies.Activities;
 
+import android.content.Context;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ContentFrameLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ListView;
 
@@ -14,22 +19,35 @@ import polina.example.com.movies.Network.NetworkConnection;
 import polina.example.com.movies.Network.OnListOfMoviesLoadListenter;
 import polina.example.com.movies.R;
 
-public class MainListActivity extends AppCompatActivity {
+public class MainListActivity extends AppCompatActivity  implements OnListOfMoviesLoadListenter{
+    final List<Movie> moviesList = new ArrayList<>();
+    private RecyclerView recyclerViewListOfMovies;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
         NetworkConnection networkConnection = new NetworkConnection(MainListActivity.this);
-        ListView listView = (ListView) findViewById(R.id.list_movies);
-        final MoviesAdapter adapter = new MoviesAdapter(this, new ArrayList<Movie>());
-        listView.setAdapter(adapter);
-        networkConnection.getListOfMovies(new OnListOfMoviesLoadListenter() {
-           @Override
-           public void onMoviesLoad(List<Movie> movies) {
-               adapter.addAll(movies);
-               adapter.notifyDataSetChanged();
-           }
-       });
+        networkConnection.getListOfMovies(this);
+        recyclerViewListOfMovies = (RecyclerView) findViewById(R.id.list_movies);
+        recyclerViewListOfMovies.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerViewListOfMovies.setLayoutManager(mLayoutManager);
+        adapter = new MoviesAdapter(this, moviesList);
+        recyclerViewListOfMovies.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onMoviesLoad(List<Movie> movies) {
+        Log.d("Array from server", movies.toString());
+        System.err.println("!!!!!!!!!!!!!!!!!! " + (Looper.getMainLooper().getThread() == Thread.currentThread()));
+        moviesList.clear();
+        moviesList.addAll(movies);
+        adapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/polina/example/com/movies/Adapters/MoviesAdapter.java
+++ app/src/main/java/polina/example/com/movies/Adapters/MoviesAdapter.java
@@ -2,10 +2,10 @@ package polina.example.com.movies.Adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -13,7 +13,6 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
-
 import polina.example.com.movies.Models.Movie;
 import polina.example.com.movies.R;
 import polina.example.com.movies.Utils.Utils;
@@ -22,54 +21,78 @@ import polina.example.com.movies.Utils.Utils;
  * Created by polina on 9/13/17.
  */
 
-public class MoviesAdapter extends ArrayAdapter<Movie> {
+public class MoviesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     Context context;
+    List<Movie> movieList;
+    private static final int SHOW_POPULAR =1;
+    private static final int SHOW_NORMAL = 2;
 
-    private static class ViewHolder{
-        ImageView image;
-        TextView title;
-        RatingBar rating;
-        TextView genre;
-        TextView description;
+    public MoviesAdapter(@NonNull Context context, @NonNull List<Movie> objects) {
+        super();
+        this.context = context;
+        movieList = objects;
     }
 
 
-    public MoviesAdapter(@NonNull Context context, @NonNull List<Movie> objects) {
-        super(context, R.layout.movie_item, objects);
-        this.context =   context;
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        Movie movie = getItem(position);
-
-        ViewHolder viewHolder;
-        if (convertView == null) {
-            viewHolder = new ViewHolder();
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate( R.layout.movie_item, parent, false);
-            viewHolder.image = (ImageView) convertView.findViewById(R.id.movie_image);
-            viewHolder.title = (TextView) convertView.findViewById(R.id.movie_title);
-            viewHolder.rating = (RatingBar) convertView.findViewById(R.id.movie_ratingBar);
-            viewHolder.genre = (TextView) convertView.findViewById(R.id.movie_genre);
-            viewHolder.description = (TextView) convertView.findViewById(R.id.movie_description);
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
+    public int getItemViewType(int position) {
+        if(movieList.get(position).getRating()>=5){
+            return SHOW_POPULAR;
         }
+         return SHOW_NORMAL;
+    }
 
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder viewHolder = new ViewHolder1(parent);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        switch (viewType) {
+            case SHOW_NORMAL:
+                View v1 = inflater.inflate(R.layout.movie_item, parent, false);
+                viewHolder = new ViewHolder1(v1);
+                break;
+            case SHOW_POPULAR:
+                View v2 = inflater.inflate(R.layout.movie_trailer_play, parent, false);
+                viewHolder = new ViewHolder2(v2);
+                break;
+        }
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        switch (holder.getItemViewType()) {
+            case SHOW_NORMAL:
+                ViewHolder1 vh1 = (ViewHolder1) holder;
+                configureViewHolder1(vh1, position);
+                break;
+            case SHOW_POPULAR:
+                ViewHolder2 vh2 = (ViewHolder2) holder;
+                configureViewHolder2(vh2, position);
+                break;
+
+        }
+    }
+
+    private void configureViewHolder2(ViewHolder2 viewHolder, int position) {
+        Movie movie = movieList.get(position);
         viewHolder.title.setText(movie.getTitle());
-        viewHolder.description.setText(movie.getDescription());
-        viewHolder.rating.setRating(movie.getRating());
+        viewHolder.rating.setRating(movie.getRating()/2);
+        String url = context.getResources().getString(R.string.image_uri) + movie.getImage_land();
+        System.err.println("URL: " + url);
+        Picasso.with(context).load(url).placeholder(R.drawable.placeholder)
+                .error(R.drawable.icon_error_).into(viewHolder.image);
+        viewHolder.genre.setText(getGenre(movie));
+    }
 
-        if(Utils.isLandOrientation(context)){
-            Picasso.with(context).load(context.getResources().getString(R.string.image_uri)+movie.getImage_land()).placeholder(R.drawable.placeholder)
-                    .error(R.drawable.icon_error_).into(viewHolder.image);
-        } else {
-            Picasso.with(context).load(context.getResources().getString(R.string.image_uri) + movie.getImage_port()).placeholder(R.drawable.placeholder)
-                    .error(R.drawable.icon_error_).into(viewHolder.image);
-        }
-
+    private String getGenre(Movie movie) {
         String genre = "";
         for(int i = 0; i<movie.getGenre().size(); i++){
             if(i==movie.getGenre().size()-1){
@@ -78,9 +101,59 @@ public class MoviesAdapter extends ArrayAdapter<Movie> {
                 genre = genre + Utils.getGenre(movie.getGenre().get(i)) + ", ";
             }
         }
-        viewHolder.genre.setText(genre);
-        return convertView;
+        return genre;
     }
+
+    private void configureViewHolder1(ViewHolder1 viewHolder, int position) {
+        Movie movie = movieList.get(position);
+        viewHolder.title.setText(movie.getTitle());
+        viewHolder.description.setText(movie.getDescription());
+        viewHolder.rating.setRating(movie.getRating()/2);
+        String url = context.getResources().getString(R.string.image_uri) +
+                (Utils.isLandOrientation(context) ? movie.getImage_land() : movie.getImage_port());
+        System.err.println("URL: " + url);
+        Picasso.with(context).load(url).placeholder(R.drawable.placeholder).error(R.drawable.icon_error_).into(viewHolder.image);
+        viewHolder.genre.setText(getGenre(movie));
+    }
+
+
+    public static class ViewHolder1 extends RecyclerView.ViewHolder {
+        ImageView image;
+        TextView title;
+        RatingBar rating;
+        TextView genre;
+        TextView description;
+
+        public ViewHolder1(View itemView) {
+            super(itemView);
+            image = (ImageView) itemView.findViewById(R.id.movie_image);
+            title = (TextView) itemView.findViewById(R.id.movie_title);
+            rating = (RatingBar) itemView.findViewById(R.id.movie_ratingBar);
+            genre = (TextView) itemView.findViewById(R.id.movie_genre);
+            description = (TextView) itemView.findViewById(R.id.movie_description);
+        }
+    }
+
+        public static class ViewHolder2 extends RecyclerView.ViewHolder {
+            ImageView image;
+            TextView title;
+            RatingBar rating;
+            TextView genre;
+            TextView description;
+
+            public ViewHolder2(View itemView) {
+                super(itemView);
+                image = (ImageView) itemView.findViewById(R.id.movie_image_play);
+                title = (TextView) itemView.findViewById(R.id.movie_title_play);
+                rating = (RatingBar) itemView.findViewById(R.id.movie_ratingBar_play);
+                genre = (TextView) itemView.findViewById(R.id.movie_genre_play);
+            }
+
+
+
+        }
+
+
 
 
 }
